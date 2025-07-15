75f768b05ea19f8da1e920d859e3f0ca290f0e4e
Jackie Merritt <remmerritt@gmail.com>
2025-07-15 16:01:10 -0400

    Assignment Complete

--- Chp12-Lb3.py
+++ Chp12-Lb3.py
@@ -3,15 +3,15 @@ import requests
 
 def main():
     # https://api.openweathermap.org/data/2.5/weather?zip=43567,us&appid=002ec48f71985084985b01726b4f892d&units=imperial
-    response = requests.get("https://api.openweathermap.org/data/2.5/weather?zip=43567,us&appid=002ec48f71985084985b01726b4f892d&units=imperial")
+    code = input('Please enter your zip code: ')
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?zip={code},us&appid=002ec48f71985084985b01726b4f892d&units=imperial')
 
     if response:
         response_dict = response.json()
-        #print(response_dict["main"]["temp"])
-        #print(f'Current Weather for {response_dict["name"]}')
-        print(response_dict["weather"]["main"])
-        #print(f'Temperature: {response_dict["main"]["temp"]} Degrees\nFeels Like: {response_dict["main"]["feels_like"]} Degrees')
-        #print(f'Humidity: {response_dict["main"]["humidity"]}%\nWind: {response_dict["wind"]["speed"]} Knots @ {response_dict["wind"]["deg"]} Degrees')
+        print(f'Current Weather for {response_dict["name"]}')
+        print(f'Conditions: {response_dict["weather"][0]["main"]}')
+        print(f'Temperature: {response_dict["main"]["temp"]:.0f} Degrees\nFeels Like: {response_dict["main"]["feels_like"]:.0f} Degrees')
+        print(f'Humidity: {response_dict["main"]["humidity"]}%\nWind: {response_dict["wind"]["speed"]:.2f} Knots @ {response_dict["wind"]["deg"]} Degrees')
     
 
 
